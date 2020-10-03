2f9ec375a85a8877a83ae631a29b50f47b570003
Somfic <somfic@gmail.com>
2020-10-03 02:41:22 +0200

    Initial commit

---
+++ EliteVA/EliteVA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EliteVA
+{
+    public class EliteVA
+    {
+        public static Guid VA_Id() => new Guid("{B16F6232-5AD2-4451-BBED-C7696B41AB66}");
+        public static string VA_DisplayName() => "EliteVA";
+        public static string VA_DisplayInfo() => $"EliteVA v{version} by Somfic";
+
+        public static void VA_Init1(dynamic vaProxy)
+        {
+
+        }
+
+        public static void VA_Exit1(dynamic vaProxy)
+        {
+
+        }
+
+        public static void VA_StopCommand1()
+        {
+
+        }
+
+        public static void VA_Invoke1(dynamic vaProxy)
+        {
+
+        }
+    }
+}
