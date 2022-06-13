02f51610d2e448ab23a64b0b3b93d243a87b14e0
tbayart <tbayart@nordnet.fr>
2022-06-13 18:09:40 +0200

    Added missing VoiceAttackLog.cs and VoiceAttackColor.cs

---
+++ EliteVA.VoiceAttackProxy/Log/VoiceAttackColor.cs
@@ -0,0 +1,16 @@
+namespace EliteVA.VoiceAttackProxy.Log
+{
+    public enum VoiceAttackColor
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Orange,
+        Purple,
+        Blank,
+        Black,
+        Gray,
+        Pink
+    }
+}
---
+++ EliteVA.VoiceAttackProxy/Log/VoiceAttackLog.cs
@@ -0,0 +1,28 @@
+// EliteVA.VoiceAttackProxy.Log.VoiceAttackLog
+using System;
+using System.Threading.Tasks;
+
+namespace EliteVA.VoiceAttackProxy.Log
+{
+    public class VoiceAttackLog
+    {
+        private readonly dynamic _proxy;
+
+        internal VoiceAttackLog(dynamic proxy, IServiceProvider services = null)
+        {
+            _proxy = proxy;
+        }
+
+        public Task Write(string content, VoiceAttackColor color)
+        {
+            _proxy.WriteToLog(content, color.ToString());
+            return Task.CompletedTask;
+        }
+
+        public Task Clear()
+        {
+            _proxy.ClearLog();
+            return Task.CompletedTask;
+        }
+    }
+}
