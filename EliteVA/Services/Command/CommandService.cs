87d89c8faf47c3d3da821dfe768401f5e1264949
tbayart <tbayart@nordnet.fr>
2022-06-26 11:35:11 +0200

    Reworked CommandService : Removed unused IList, replaced WriteAllLines getting worse over time with appending commands to dated file

--- EliteVA/Services/Command/CommandService.cs
+++ EliteVA/Services/Command/CommandService.cs
@@ -3,7 +3,6 @@ using EliteVA.Constants.Paths.Abstractions;
 using EliteVA.Constants.Proxy.Abstractions;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -18,7 +17,7 @@ namespace EliteVA.Services
         private readonly IProxy _proxy;
         private readonly IPaths _paths;
         private readonly IEliteDangerousApi _api;
-        private readonly IList<(DateTime timestamp, string name, bool success)> _invokedCommands;
+        private readonly Lazy<string> _commandsPath;
         #endregion fields
 
         #region ctor
@@ -28,7 +27,7 @@ namespace EliteVA.Services
             _proxy = proxy;
             _paths = paths;
             _api = api;
-            _invokedCommands = new List<(DateTime, string, bool)>();
+            _commandsPath = new Lazy<string>(CommandPathFactory);
         }
         #endregion ctor
 
@@ -43,20 +42,33 @@ namespace EliteVA.Services
 
             if (_proxy.GetProxy().Commands.Exists(command).GetAwaiter().GetResult())
             {
-                _invokedCommands.Add((DateTime.Now, command, true));
                 _proxy.GetProxy().Commands.Invoke(command);
             }
             else
             {
-                _invokedCommands.Add((DateTime.Now, command, false));
                 _logger.LogDebug("Skipping '{Command}' because it has not been subscribed to", command);
             }
 
-            var commandsPath = Path.Combine(_paths.PluginDirectory.FullName, "Commands");
-            Directory.CreateDirectory(commandsPath);
-
-            File.WriteAllLines(Path.Combine(commandsPath, "InvokedCommands.txt"), _invokedCommands.Select(x => $"{x.timestamp.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture)} {x.name}"));
+            AppendInvokedCommands(command);
         }
         #endregion ICommandService
+
+        #region methods
+        private string CommandPathFactory()
+        {
+            var commandsPath = Path.Combine(_paths.PluginDirectory.FullName, "Commands");
+            Directory.CreateDirectory(commandsPath);
+            return commandsPath;
+        }
+
+        private void AppendInvokedCommands(params string[] commands)
+        {
+            var now = DateTime.Now;
+            var date = now.ToString("yyyy-MM-dd");
+            var time = now.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture);
+            var commandsPath = Path.Combine(_commandsPath.Value, $"InvokedCommands_{date}.txt");
+            File.AppendAllLines(commandsPath, commands.Select(o => $"{time} {o}"));
+        }
+        #endregion methods
     }
 }
