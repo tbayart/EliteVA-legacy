5962c3e60f3256491c5df9cd54dba2c232047bb2
tbayart <tbayart@nordnet.fr>
2022-08-31 20:41:05 +0200

    Added GUI unit test to test landing pads show/hide

---
+++ Tests/EliteVA.Tests/GUI.cs
@@ -0,0 +1,34 @@
+﻿using EliteVA.GUI;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EliteVA.Tests
+{
+    public class GUI
+    {
+        [Fact]
+        public async Task ShowHideWindow()
+        {
+            var logger = Mock.Of<ILogger<WindowManager>>();
+            var wm = new WindowManager(logger);
+            var coords = new LandingPadsCoords();
+
+            Console.WriteLine("Window should be shown while landing pads are iterated");
+            foreach (var stationType in coords.StationTypes)
+            {
+                var padCount = coords.GetPadCount(stationType);
+                for (int padNumber = 1; padNumber <= padCount; padNumber++)
+                {
+                    wm.LandingPadShow($"Test {stationType}", stationType, padNumber);
+                    await Task.Delay(500);
+                }
+            }
+            wm.LandingPadHide();
+            Console.WriteLine("Window should now be hidden");
+            await Task.Delay(1000);
+        }
+    }
+}
